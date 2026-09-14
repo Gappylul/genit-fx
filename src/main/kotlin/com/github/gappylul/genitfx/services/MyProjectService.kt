49e6c35a3f856a147a783e94664178b38b6fbe51
GitHub Action <action@github.com>
2026-09-14 15:17:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/gappylul/genitfx/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gappylul/genitfx/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.gappylul.genitfx.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gappylul.genitfx.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/gappylul/genitfx/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/gappylul/genitfx/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.gappylul.genitfx.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
